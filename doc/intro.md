ac5cdb2b11ed139496994d95b9ba17a45e5e616b
wegfawefgawefg <668es218pur@gmail.com>
2018-01-31 22:25:18 -0600

    init comit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to perfectplaylist
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/perfectplaylist/core.clj
@@ -0,0 +1,59 @@
+;------------------------------;
+;  THE PERFECT PLAYLIST MAKER  ;
+;------------------------------;
+;                              ;
+;  -----  HOW IT WORKS  -----  ;
+;                              ;
+;Provide a desired playlist    ; 
+;length in seconds, and a list ;
+;of songs...                   ;
+;A perfect playlist will be    ;
+;generated for you!            ;
+;                              ;
+;------------------------------;
+
+(ns perfectplaylist.core
+  (:gen-class))
+
+(defn getAllStdin []
+  (take-while identity (repeatedly #(read-line))))
+
+(defn tokenizeLines [lines]
+  "join lines into one big line, then tokenize the big line"
+  (let [bigLine (clojure.string/join '" " lines)]
+    (clojure.string/split bigLine #" ")))
+
+(defn parse-int [s]
+   (Integer. (re-find  #"\d+" s )))
+
+(defn tokensToSongDurationMap [tokens]
+  ;put tokens into map; ( k v k v k v) -> { k v, k v}
+  (let [songDurationMap (apply hash-map tokens)]
+    ;convert all the values to numbers
+    (apply merge (map (fn [[k v]] {k (parse-int v) }) songDurationMap))))
+
+(defn createPlaylist [namesAndDurations targetDuration]
+  "add random songs to playlist until no more songs can fit in the target duration"
+  (loop [playlist {} viableSongs namesAndDurations]
+    (let [currentDuration (reduce + (vals playlist))]
+      (let [durationGap (- targetDuration currentDuration)]
+        (let [viableSongs  ;remove songs too long to add to the playlist
+              (into (hash-map) (filter #(< (second %) durationGap) viableSongs ))]
+          ;are there any songs short enough to add to the playlist?
+          (if (zero? (count viableSongs))
+            playlist
+            ;pick random song
+            (let [randomSongName (rand-nth (keys viableSongs))]
+              (recur 
+               ;add the random song to the playlist
+               (assoc playlist randomSongName (get viableSongs randomSongName))
+               ;remove the chosen song from the list of remaining songs
+               (dissoc viableSongs randomSongName)))))))))
+
+(defn -main [& args]
+  (let [targetDuration (parse-int (first args))]
+    (doseq [key (shuffle (keys (createPlaylist 
+                                (tokensToSongDurationMap
+                                 (tokenizeLines (getAllStdin))) targetDuration)))]
+      (print key " ")))
+  (println))
